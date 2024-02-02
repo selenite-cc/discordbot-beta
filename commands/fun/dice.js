d9264c90efb0600bb59fc15a698ca600206c9e37
a456pur <bookboi09@gmail.com>
2024-02-02 18:49:35 +1000

    totally needed important changes

---
+++ commands/fun/dice.js
@@ -0,0 +1,22 @@
+const { EmbedBuilder } = require("discord.js");
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("roll")
+    .setDescription("Roll a 6-sided dice!"),
+  async execute(interaction) {
+    try {
+      const result = Math.floor(Math.random() * 6) + 1;
+
+      let rollEmbed = new EmbedBuilder()
+        .setTitle(`🎲 **${result}**`)
+        .setColor("#0099ff")
+
+      await interaction.reply({ embeds: [rollEmbed] });
+    } catch (error) {
+      console.error(error);
+      await interaction.reply("There was an error. Please ping the owner if you see this.");
+    }
+  },
+};
